311fe3406ac3ed7705bd9678b6e910d319f129e9
reddingk <kris.redding3@gmail.com>
2019-01-10 19:10:32 -0500

    fix build

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-//import { hydrate, render } from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import App from './js/app.js';
 
-import ReactDOM from 'react-dom';
-ReactDOM.render(<App />, document.getElementById('app'));
+//import ReactDOM from 'react-dom';
+//ReactDOM.render(<App />, document.getElementById('app'));
 
-/*const rootElement = document.getElementById("app");
+const rootElement = document.getElementById("app");
 if (rootElement.hasChildNodes()) {
   hydrate(<App />, rootElement);
 } else {
   render(<App />, rootElement);
-}*/
+}
